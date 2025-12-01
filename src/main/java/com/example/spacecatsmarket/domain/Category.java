1dccfc26b6e945b3e6ebdc699da2f2fe165924e6
Sabadash Viktoriia <sabadashvika2006@gmail.com>
2025-12-01 23:09:20 +0200

    дфи4

--- src/main/java/com/example/spacecatsmarket/domain/Category.java
+++ src/main/java/com/example/spacecatsmarket/domain/Category.java
@@ -1,13 +1,23 @@
 package com.example.spacecatsmarket.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
+import java.util.List;
 
-import java.util.UUID;
-
+@Entity
+@Table(name = "categories")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Category {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categories_seq")
+    @SequenceGenerator(name = "categories_seq", sequenceName = "categories_seq", allocationSize = 1)
+    private Long id;
+
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
