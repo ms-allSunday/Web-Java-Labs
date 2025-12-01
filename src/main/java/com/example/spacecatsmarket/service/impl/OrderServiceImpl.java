388666cf16fdebb010c1ceb3846d414c75fd8d97
Sabadash Viktoriia <sabadashvika2006@gmail.com>
2025-12-01 23:23:46 +0200

    лаб4-2

--- src/main/java/com/example/spacecatsmarket/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/spacecatsmarket/service/impl/OrderServiceImpl.java
@@ -31,6 +31,9 @@ public class OrderServiceImpl implements OrderService {
     @Override
     @Transactional
     public Order createOrder(Order order) {
+
+        order.setOrderNumber(java.util.UUID.randomUUID());
+        
         if (order.getOrderDate() == null) {
             order.setOrderDate(java.time.LocalDateTime.now());
         }
